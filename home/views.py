4a08a886c09eb30623e8daf3ff74873baf7a4aff
Jeemyeong Lee <ljm901011@gmail.com>
2016-04-25 22:46:41 +0900

    Update views.py
    mail update

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 from django.shortcuts import render
 from django.utils import timezone
 from django.core.urlresolvers import reverse
@@ -7,16 +8,15 @@ from .models import Activity
 from django.core.mail import EmailMessage, send_mail
 
 def home(request):
-	activities = Activity.objects.all()
-	return render(request, 'home/index.html', {'activities': activities})
+    activities = Activity.objects.all()
+    return render(request, 'home/index.html', {'activities': activities})
 
 def mail(request):
     name= request.POST['name']
     fromEmail= request.POST['email']
     message= request.POST['message']
-    contents =fromEmail+'to depromeet :  '+message
     toEmail = 'deproapply@gmail.com'
-    email = EmailMessage('questions from '+ name, message, fromEmail, to=[toEmail])
+    email = EmailMessage(name+'님이 보내주신 메일입니다.'.decode('utf-8'), message+'\n\n'+'sent from depromeet.com :-)'.decode('utf-8'), fromEmail, to=[toEmail])
     email.send()
     return HttpResponseRedirect(reverse('home'))
 
